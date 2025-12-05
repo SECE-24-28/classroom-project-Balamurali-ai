bb5e9fb404c8bf32e30a1e8d86f9a8c2da32fab8
Balamurali S <balamurali.s2024aids@sece.ac.in>
2025-12-05 12:40:49 +0530

    day 5

---
+++ day5/script.js
@@ -0,0 +1,10 @@
+// document.body.style.backgroundColor = "green";
+// document.body.style.fontSize = "50px";document.body.style.backgroundColor = "#0e2f4dff";
+// document.body.style.color = "white";
+// const uname = "john_doe";
+
+// document.body.innerHTML += "<h2>Welcome to JavaScript Programming!</h2>";
+// document.body.style.fontSize = "20px";
+
+// document.title = ${uname}-dashboard;
+const cons = document.get
